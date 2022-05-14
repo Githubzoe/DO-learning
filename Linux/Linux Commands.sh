a5257cb3375a6cd1fe54a277df38bb61cd9c4163
afzaal ahmad <afzaaluet@gmail.com>
2022-05-15 02:17:41 +0500

    15-02-2022-2

--- Linux/Linux Commands.sh
+++ Linux/Linux Commands.sh
@@ -9,3 +9,7 @@ create symblic link for very long path
 
 ln -s /home/afzaal/afzaal-devops-repo-github-2022/afzaal-devops ~/devops
 
+ln -s /home/afzaal/afzaal-devops-repo-github-2022/Global-financial-media/mylearningfiles ~/mylearningfiles
+
+ ls -l  # check weather given file is a sybloic link
+
