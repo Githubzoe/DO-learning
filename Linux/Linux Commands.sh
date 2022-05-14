d553e7cbf9336019f5156cace151d5549144b10e
Afzaal Ahmad <afzaaluet@gmail.com>
2022-05-14 23:46:42 +0500

    Pushing from home laotop-14-05-2022

--- Linux/Linux Commands.sh
+++ Linux/Linux Commands.sh
@@ -3,3 +3,9 @@ Linux Commands
 SSH Private key location
 
 /home/johndoe/.ssh/id_rsa
+
+
+create symblic link for very long path
+
+ln -s /home/afzaal/afzaal-devops-repo-github-2022/afzaal-devops ~/devops
+
